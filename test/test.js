2124133745e0040aac4aa459ee87f1f3d03e4815
Pierre TOP (TOPI) <ptop@octo.com>
2025-02-13 16:52:41 +0100

    Add test framework

---
+++ test/test.js
@@ -0,0 +1,15 @@
+import { expect } from 'chai';
+
+describe('Unit - index.js',()=>{
+    context('when we start',()=>{
+        it('should fail',()=>{
+            // given
+
+            // when
+            const actual = false;
+
+            // then
+            expect(actual).to.be.true;
+        })
+    })
+})
